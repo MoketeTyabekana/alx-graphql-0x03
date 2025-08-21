74258682ad06eb934899332e4f79044e89ee1228
Mokete <ematyabekana@gmail.com>
2025-08-21 20:24:14 +0200

    chore: added Sentry for error tracking and monitoring

--- alx-rick-and-morty-app/components/ErrorProneComponent.tsx
+++ alx-rick-and-morty-app/components/ErrorProneComponent.tsx
@@ -1,5 +1,14 @@
-const ErrorProneComponent: React.FC = () => {
-  throw new Error('This is a test error!');
-};
+import React from 'react';
+import * as Sentry from '@sentry/react';
+
+class ErrorProneComponent extends React.Component {
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    Sentry.captureException(error, errorInfo);
+  }
+
+  render() {
+    return <div>This is a test error! (Replace with your fallback UI)</div>;
+  }
+}
 
 export default ErrorProneComponent;
